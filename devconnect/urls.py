2df9ac10b4297abea1cb2bdcff675d5defb4cc28
Jaechul Jung <johnnynv6@gmail.com>
2022-07-09 15:39:58 +0900

    Template render, inheritance complete

--- devconnect/urls.py
+++ devconnect/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Go into projects folder and include urls.py in that folder.
+    path('', include('projects.urls'))
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def projects(request):
+    return render(request, 'projects/projects.html')
+
+
+def project(request, pk):
+    return render(request, 'projects/single-project.html')
