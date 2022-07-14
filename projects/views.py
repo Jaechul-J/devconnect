a49cc7c671f9018aa1edabadaf2e9a3bb7c2293a
Jaechul Jung <johnnynv6@gmail.com>
2022-07-14 23:14:34 +0900

    Databse Queries complete

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 projectsList = [
     {
         'id': '1',
@@ -22,15 +22,11 @@ projectsList = [
 
 
 def projects(request):
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
